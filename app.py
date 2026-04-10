c57a3cf46a068c93895a7c2c1412b56fb0873068
DheerajCEO <dheerunb09@gmail.com>
2026-04-10 15:53:28 +0200

    fix secrets for streamlit cloud

--- app.py
+++ app.py
@@ -1,13 +1,9 @@
 import streamlit as st
-import os
 import google.generativeai as genai
 from supabase_client import supabase
-from dotenv import load_dotenv
 from datetime import date
 
-load_dotenv()
-
-genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+genai.configure(api_key=st.secrets["GEMINI_API_KEY"])
 model = genai.GenerativeModel("gemini-2.5-flash")
 st.title("💰 Finance App")
 
